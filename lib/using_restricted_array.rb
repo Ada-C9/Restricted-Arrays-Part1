03ffa8d0092c034a4d99cdb35a6a9c1d2d6a1681
Nora Peters <norapeters5@gmail.com>
2018-03-09 12:16:36 -0800

    commented out tests in spec file that i'm not currently testing. completed length method with test passing.

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -7,6 +7,11 @@ require_relative 'restricted_array.rb'
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
 def length(array)
+  i = 0
+  while array[i] != nil
+    i += 1
+  end
+  return i
   raise NotImplementedError
 end
 
